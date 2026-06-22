67dd149d7ee696863f2d72207af32acb5919387f
benmandrew <benmandrew@gmail.com>
2026-06-22 21:25:46 +0100

    bench: add mutate_specification benchmark

---
+++ bench/bench_mutation.cpp
@@ -0,0 +1,29 @@
+#include <benchmark/benchmark.h>
+
+#include "genetic/mutation.hpp"
+#include "genetic/random_source.hpp"
+#include "requirement.hpp"
+
+namespace {
+
+static void BenchMutateSpecification(benchmark::State& state) {
+    const Specification spec(
+        {},
+        {
+            Requirement(Formula("true"), Formula("takeoff_roll"),
+                        timing::for_ticks(5)),
+            Requirement(Formula("true"), Formula("!(takeoff_roll) & lift_off"),
+                        timing::within_ticks(5)),
+            Requirement(Formula("!(takeoff_roll)"), Formula("lift_off"),
+                        timing::after_ticks(1)),
+        },
+        {"takeoff_roll"}, {"lift_off"});
+    const RandomSource rng = make_random_source_from_seed(42);
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(mutate_specification(spec, rng));
+    }
+}
+// NOLINTNEXTLINE(cert-err58-cpp)
+BENCHMARK(BenchMutateSpecification);
+
+}  // namespace

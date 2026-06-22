ee81f0a581b9735fe7a6b1ebecf9e9ac6179f4b7
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 20:21:16 +0000

    add counter benchmarks (googlecpp) benchmark result for f31b92b084174b1a84907a2856a222c10de111aa

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1782159676544,
+  "repoUrl": "https://github.com/benmandrew/counter",
+  "entries": {
+    "counter benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "benmandrew@gmail.com",
+            "name": "benmandrew",
+            "username": "benmandrew"
+          },
+          "committer": {
+            "email": "benmandrew@gmail.com",
+            "name": "benmandrew",
+            "username": "benmandrew"
+          },
+          "distinct": true,
+          "id": "f31b92b084174b1a84907a2856a222c10de111aa",
+          "message": "fix: move benchmark index.html to gh-pages root",
+          "timestamp": "2026-06-22T21:16:40+01:00",
+          "tree_id": "1dd5262373f2c1eb366329c233278c3b6990bbb8",
+          "url": "https://github.com/benmandrew/counter/commit/f31b92b084174b1a84907a2856a222c10de111aa"
+        },
+        "date": 1782159675949,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BenchSpecImpliesWarmCache",
+            "value": 678.3572238813823,
+            "unit": "ns/iter",
+            "extra": "iterations: 1032319\ncpu: 678.2988688573978 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
